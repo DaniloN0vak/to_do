d11b2d92bbbc4bea2e9cb90966d4e6206d0e8878
DanyloNovak <danilonovak0@gmail.com>
2025-05-20 13:03:53 +0300

    Add Views and Controller

---
+++ To-do_list/Controllers/TasksController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace To_do_list.Controllers
+{
+    public class TasksController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        // 
+        // GET: /HelloWorld/Welcome/ 
+        public string Welcome()
+        {
+            return "This is the Welcome action method...";
+        }
+    }
+}
